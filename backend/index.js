3b700dfcf08ff6d70f5fe0c4275921738fdea823
moralesmj <moralesmjdev@gmail.com>
2026-02-20 11:59:48 -0600

    changing end points

--- backend/index.js
+++ backend/index.js
@@ -24,10 +24,6 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
