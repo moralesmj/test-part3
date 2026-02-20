dad3c5c37d87cbea55b2f889e4cb68645bf09651
moralesmj <moralesmjdev@gmail.com>
2026-02-20 12:19:19 -0600

    fixing issues

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,10 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
