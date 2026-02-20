1d1e09dbe587fa2857d21a7a2e6862ac270bfe06
moralesmj <moralesmjdev@gmail.com>
2026-02-20 14:13:23 -0600

    fixing issues

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,18 @@ let notes = [
     },
 ]
 
-app.use(express.json())
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
 app.use(cors())
 app.use(express.static('dist'))
+app.use(express.json())
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -75,6 +84,12 @@ app.delete('/api/notes/:id', (request, response) => {
 
     response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
